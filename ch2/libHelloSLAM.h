0a41b781cb44680041b65719c9f0075ce38bb497
Alexander Abramenko <mr.abramenko@yandex.ru>
2023-11-10 18:00:52 +0300

    ch2: translate

--- ch2/libHelloSLAM.h
+++ ch2/libHelloSLAM.h
@@ -1,8 +1,8 @@
 #ifndef LIBHELLOSLAM_H_
 #define LIBHELLOSLAM_H_
-// 上面的宏定义是为了防止重复引用这个头文件而引起的重定义错误
+//The above macro definition is to prevent redefinition errors caused by repeatedly referencing this header file.
 
-// 打印一句hello的函数
+// Function to print a hello sentence
 void printHello();
 
 #endif
--- ch2/useHello.cpp
+++ ch2/useHello.cpp
@@ -1,6 +1,6 @@
 #include "libHelloSLAM.h"
 
-// 使用 libHelloSLAM.h 中的 printHello() 函数
+//Use the printHello() function in libHelloSLAM.h
 int main(int argc, char **argv) {
   printHello();
   return 0;
